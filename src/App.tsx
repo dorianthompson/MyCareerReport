64343f718f48892cbcf5fd2ed78e0c0dea9a72a9
Dorian <thompsondorian2@gmail.com>
2025-06-07 22:23:10 -0400

    debug Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css';
 
+import { HashRouter as Router,Routes, Route } from 'react-router-dom';
+
+import BasicQuiz from './components/BasicQuiz';
+import DetailedQuiz from './components/DetailedQuiz';
+import CareerReport from './components/CareerReport';
 import HomePage from './components/HomePage';
 function App() {
  
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/basicquiz" element={<BasicQuiz/>} />
+          <Route path="/detailedquiz" element={<DetailedQuiz/>} />
+          <Route path="/results" element={<CareerReport/>} />
+        </Routes>
+    </Router>
   );
 }
 
--- src/components/BasicQuiz.tsx
+++ src/components/BasicQuiz.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function BasicQuiz() {
   return (
     <div>
